b5f38b803de243a1805046088b915f86cce4be6c
Joel Waymack <joel.waymack@microsoft.com>
2022-06-15 09:28:03 -0500

    Add App Insights telemetry.

--- Movies/App_Start/FilterConfig.cs
+++ Movies/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace Movies
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
